4e700cd2c80236e29d68b33b8a5c2d3dfff10476
Oren Shalev <orenico4@gmail.com>
2023-11-27 18:27:22 +0200

    supporting quality param and returning a json object

--- functions/main.py
+++ functions/main.py
@@ -6,36 +6,45 @@ from firebase_functions import https_fn, options
 from firebase_admin import initialize_app
 from pytube import YouTube
 from urllib.parse import unquote
+import json
 
 initialize_app()
 
-
-@https_fn.on_request()
-def on_request_example(req: https_fn.Request) -> https_fn.Response:
-    return https_fn.Response("Hello world!")
-
 @https_fn.on_request(cors=options.CorsOptions(cors_origins="*", cors_methods=["get", "post"]))
 def get_download_url(req: https_fn.Request) -> https_fn.Response:
-    """Take the text parameter passed to this HTTP endpoint and insert it into
-    a new document in the messages collection."""
-    # Grab the text parameter.
-
+   
+    # grab the args
     encodedOriginalUrl = req.args.get('url')
-    print('url', encodedOriginalUrl)
+    quality = req.args.get('quality')
+
     if encodedOriginalUrl is None:
         return https_fn.Response("No url parameter provided", status=400)
     
+    if quality is None:
+        quality = '720p'
+
+    # the url is sent encoded so we need to decode it
     originalUrl = unquote(encodedOriginalUrl)
 
-    downloadUrl = getDownloadUrl(originalUrl, '720p')
-    print(originalUrl)
-    # Send back a message that we've successfully written the message
-    return https_fn.Response(f"Download Url is: {downloadUrl}")
+    # get a downloadable url
+    downloadUrl = getDownloadUrl(originalUrl, quality)
+
+    # return the downloadable url in the response
+    responseObject = json.dumps({'url': downloadUrl})
+
+    return https_fn.Response(responseObject)
 
 
 def getDownloadUrl(url, resolution = '360p'):
+        print('resolution', resolution)
         streams = YouTube(url).streams
         for stream in streams:
             if (stream.mime_type == 'video/mp4' and stream.resolution == resolution and stream.is_progressive == True):
                 return stream.url
 
+class Content:
+  def __init__(self, url):
+    self.url = url
+
+  def __str__(self) -> str:
+      return f'content url is: {self.url}'  
